0c7f7e5ccb0631ae8628027a733a87674b3b887d
Nice <anupong.niceone@gmail.com>
2022-05-16 01:06:10 +0700

    dev

--- components/TopChart/index.tsx
+++ components/TopChart/index.tsx
@@ -1,4 +1,4 @@
-import { Key } from "react"
+import { Key } from "react";
 import React, { useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -8,42 +8,64 @@ const TopChart: React.FC<any> = ({ channels, total }) => {
 
     const getMorePost = async () => {
         const res = await fetch(
-          `https://api.holotools.app/v1/channels?offset=${data.length}&_limit=10`
+            `https://api.holotools.app/v1/channels?offset=${data.length}&_limit=10`
         );
         const { channels } = await res.json();
         setCharts((data: any) => [...data, ...channels]);
-        if(total==data.length){
-            setHasMore(false)
+        if (total == data.length) {
+            setHasMore(false);
         }
     };
 
     return (
-    <div className="w-full mt-5 space-y-2">
+        <div className="w-full mt-5 space-y-2">
             <div className="px-4">
-                <div className="font-medium text-lg">
-                    Top Chart
-                </div>
+                <div className="font-medium text-lg">Top Chart</div>
                 <div className="flex space-x-2">
+                    
+                    <div className="dropdown">
+                        <label tabindex="0" className="btn m-1">
+                            Click
+                        </label>
+                        <ul
+                            tabindex="0"
+                            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+                        >
+                            <li>
+                                <a>Item 1</a>
+                            </li>
+                            <li>
+                                <a>Item 2</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
                     <div className=""> Thailand </div>
                     <div className=""> All </div>
                 </div>
             </div>
 
-        <InfiniteScroll className="w-full mt-5 space-y-2" dataLength={data?.length} next={getMorePost} hasMore={hasMore} loader={<h3> Loading...</h3>} endMessage={<h4></h4>}>
-            {data.map((channel: { id: Key | null | undefined, name: string })=>{
-                return(
-                    <div key={channel.id} className="space-y-1">
-                        <div className="flex justify-between px-[5%] items-center bg-black bg-opacity-20 rounded-md h-[100px] w-full text-2xl">
-                            <div className="">{channel.id}</div>
-                            <div>{channel.name}</div>
+            <InfiniteScroll
+                className="w-full mt-5 space-y-2"
+                dataLength={data?.length}
+                next={getMorePost}
+                hasMore={hasMore}
+                loader={<button className="btn loading">loading</button>}
+                endMessage={<h4></h4>}
+            >
+                {data.map((channel: { id: Key | null | undefined; name: string }) => {
+                    return (
+                        <div key={channel.id} className="space-y-1">
+                            <div className="flex justify-between px-[5%] items-center bg-black bg-opacity-20 rounded-md h-[100px] w-full text-2xl">
+                                <div className="">{channel.id}</div>
+                                <div>{channel.name}</div>
+                            </div>
                         </div>
-                    </div>
-                )
-            })}
-
-        </InfiniteScroll>   
-    </div>
-    )
-}
+                    );
+                })}
+            </InfiniteScroll>
+        </div>
+    );
+};
 
 export default TopChart;
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,55 +1,50 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
+import type { NextPage } from "next";
+import Head from "next/head";
 
-import TopChart from '../components/TopChart';
+import TopChart from "../components/TopChart";
 
 export const getServerSideProps = async () => {
-    const data = await fetch('https://api.holotools.app/v1/channels?limit=10').then((res)=>(res.json()));
-    return {
-      props: {data}, // will be passed to the page component as props
-    }
-}
+  const data = await fetch(
+    "https://api.holotools.app/v1/channels?limit=10"
+  ).then((res) => res.json());
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
+};
 
-const Home: NextPage<any> = ({data}) => {
-
-    return (
-        <div className=''>
-            <Head>
-                <title>Home / Vtuber Ranking</title>
-            </Head>
-            <div className=''>
-                <div className='flex'>
-
-                    <div className='sm:w-[85vw] sm:max-w-[600px] w-screen min-h-screen flex-col justify-start items-center '>
-                        <div className='bg-black rounded-b-md sticky top-0 bg-opacity-20 h-[50px] w-full backdrop-blur'>
-                            <div className='flex justify-between items-center h-full px-4  font-bold text-lg'>
-                                <div>Home</div>
-                            </div>
-                        </div>
-                        
-                        <TopChart channels={data?.channels} total={data?.total} />
-
-                        
-                    </div>
-
-                    <div className='lg:inline-flex hidden mx-4 flex-col w-[25vw] max-w-[350px]'>
-                        <div className='bg-black bg-opacity-20 h-[50px] rounded-full w-full backdrop-blur sticky top-0 z-10 flex justify-center items-center'> Search</div>
-                        <div className='sticky top-20 flex-col'>
-                            <div className='bg-black bg-opacity-20 w-full h-[500px] rounded-md'>
-                                <div className='flex justify-center items-start'>
-                                    Short Cut
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
+const Home: NextPage<any> = ({ data }) => {
+  return (
+    <div className="">
+      <Head>
+        <title>Home / Vtuber Ranking</title>
+      </Head>
+      <div className="">
+        <div className="flex">
+          <div className="sm:w-[85vw] sm:max-w-[600px] w-screen min-h-screen flex-col justify-start items-center ">
+            <div className="bg-black rounded-b-md sticky top-0 bg-opacity-20 h-[50px] w-full backdrop-blur">
+              <div className="flex justify-between items-center h-full px-4  font-bold text-lg">
+                <div>Home</div>
+              </div>
             </div>
 
+            <TopChart channels={data?.channels} total={data?.total} />
+          </div>
 
-
+          <div className="lg:inline-flex hidden mx-4 flex-col w-[25vw] max-w-[350px]">
+            <div className="bg-black bg-opacity-20 h-[50px] rounded-full w-full backdrop-blur sticky top-0 z-10 flex justify-center items-center">
+              {" "}
+              Search
+            </div>
+            <div className="sticky top-20 flex-col">
+              <div className="bg-black bg-opacity-20 w-full h-[500px] rounded-md">
+                <div className="flex justify-center items-start">Short Cut</div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
